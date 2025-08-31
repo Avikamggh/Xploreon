7a03f525e545ddac84be2dcac75d344d9b2580db
Avikamggh <physicsbyavikam@gmail.com>
2025-08-31 09:22:10 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";   // <-- this line is required to pull in Tailwind + globals.css
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
