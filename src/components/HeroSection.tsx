9a3af4aa13933e2fa5a8f9aafd10e386e0a824e8
Avikamggh <physicsbyavikam@gmail.com>
2025-09-01 09:40:40 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,7 +10,7 @@ export function HeroSection() {
       {/* Background */}
       <div className="absolute inset-0">
         <img src={bg} alt="Rocket launch" className="w-full h-full object-cover" />
-        <div className="absolute inset-0 bg-gradient-to-b from-black/70 via-black/50 to-black/80" />
+        
       </div>
 
       {/* Star specks */}
