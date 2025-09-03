1199ac981ed33a8857e36c9d4166159318062daf
Avikamggh <physicsbyavikam@gmail.com>
2025-09-03 11:14:55 +0530

    Update Competitions.tsx

--- src/components/Competitions.tsx
+++ src/components/Competitions.tsx
@@ -61,7 +61,7 @@ export default function Competitions() {
     <div className="relative min-h-screen bg-black text-white overflow-hidden">
       {/* Starfield Background */}
       <div className="absolute inset-0 -z-10">
-        <StarfieldInline />
+         <StarfieldInline density={800} />
       </div>
 
       {/* Hero */}
